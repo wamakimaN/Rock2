622ff8ee9b8f3ef96449c75b0dc643ed3c31d30f
ian1017 <mdawidamengo@gmail.com>
2020-01-23 16:27:46 +0300

    add login and logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,3 +17,23 @@ def signup():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/signup.html',form = form)
+
+#login route
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+    title = "Login"
+    return render_template('auth/login.html', form=form, title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
