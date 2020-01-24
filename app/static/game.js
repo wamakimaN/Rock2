12515fb2097bd33bee9a5ff603115229abbd7ad5
wamakimaN <wamamoses@gmail.com>
2020-01-24 05:22:59 +0300

    add array for results

--- app/static/game.js
+++ app/static/game.js
@@ -8,7 +8,16 @@ $(document).ready(function() {
   let playerOption = $("#playerOption option:selected").text();
 
   let choiceStack = ['paper', 'rock', 'scissors'];
-  let userChoiceIndex = choiceStack.indexOf(playerOption);
+  let playerChoiceIndex = choiceStack.indexOf(playerOption);
+
+
+  const results = [
+['t', 'c', 'u'],
+['u', 't', 'c'],
+['c', 'u', 't'],
+];
+
+  let playerResult = results[randomNum][playerChoiceIndex];
  
 
 
