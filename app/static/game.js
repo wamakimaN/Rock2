221e49d6e4ca97295d4618254373629c33e88968
wamakimaN <wamamoses@gmail.com>
2020-01-24 05:24:47 +0300

    define the computer's choice

--- app/static/game.js
+++ app/static/game.js
@@ -1,26 +1,34 @@
+
+$(document).ready(function () {
+
+
+
+let playerOption = $("#playerOption option:selected").text();
+
 let choiceStack = ['paper', 'rock', 'scissors'];
+let playerChoiceIndex = choiceStack.indexOf(playerOption);
 
+//define computer's choice
+let randomNum = Math.floor(Math.random() * 3);
+let computerChoice = choiceStack[randomNum];
 
-
-$(document).ready(function() {
-
-
-  let playerOption = $("#playerOption option:selected").text();
-
-  let choiceStack = ['paper', 'rock', 'scissors'];
-  let playerChoiceIndex = choiceStack.indexOf(playerOption);
-
-
-  const results = [
-['t', 'c', 'u'],
-['u', 't', 'c'],
-['c', 'u', 't'],
+// create 2D array for possible outcomes
+const results = [
+  ['t', 'c', 'u'],
+  ['u', 't', 'c'],
+  ['c', 'u', 't'],
 ];
 
-  let playerResult = results[randomNum][playerChoiceIndex];
- 
+
+let playerResult = results[randomNum][playerChoiceIndex];
+
+console.log(playerResult)
 
 
 
+
+
+
+})
 
 });
