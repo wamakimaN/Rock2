92017159a231069dee10f5955022aeaea6a88351
ian1017 <mdawidamengo@gmail.com>
2020-01-24 10:42:25 +0300

    rewrite auth for login

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,7 +24,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is not None and user.verify_password(form.password.data):
+        if user is not None:
             login_user(user, form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or Password')
