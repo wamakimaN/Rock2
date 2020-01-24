fa7311e2f5856a9f3c90ce3ae04d4bd94a184d99
wamakimaN <wamamoses@gmail.com>
2020-01-24 05:25:59 +0300

    complete game logic

--- app/static/game.js
+++ app/static/game.js
@@ -1,34 +1,34 @@
-
 $(document).ready(function () {
 
+  $("form#").submit(function (event) {
+    event.preventDefault()
+
+    let playerOption = $("#playerOption option:selected").text();
+
+    let choiceStack = ['paper', 'rock', 'scissors'];
+    let playerChoiceIndex = choiceStack.indexOf(playerOption);
+
+    //define computer's choice
+    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice = choiceStack[randomNum];
+
+    // create 2D array for possible outcomes
+    const results = [
+      ['t', 'c', 'u'],
+      ['u', 't', 'c'],
+      ['c', 'u', 't'],
+    ];
 
 
-let playerOption = $("#playerOption option:selected").text();
+    let playerResult = results[randomNum][playerChoiceIndex];
 
-let choiceStack = ['paper', 'rock', 'scissors'];
-let playerChoiceIndex = choiceStack.indexOf(playerOption);
-
-//define computer's choice
-let randomNum = Math.floor(Math.random() * 3);
-let computerChoice = choiceStack[randomNum];
-
-// create 2D array for possible outcomes
-const results = [
-  ['t', 'c', 'u'],
-  ['u', 't', 'c'],
-  ['c', 'u', 't'],
-];
-
-
-let playerResult = results[randomNum][playerChoiceIndex];
-
-console.log(playerResult)
+    console.log(playerResult)
 
 
 
 
 
 
-})
+  })
 
 });
